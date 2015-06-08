3fdca2ff5c8a1d8d3086ca9ef7a64245a39501d2
Paul Kennedy <paulk85@outlook.com>
2015-06-08 18:46:10 +0100

    Additional test and refactoring for product B

--- Kata.Cart.Tests/CheckoutTests.cs
+++ Kata.Cart.Tests/CheckoutTests.cs
@@ -14,5 +14,14 @@ namespace Kata.Cart.Tests
 
             Assert.AreEqual(50, cart.GetTotal());
         }
+
+        [Test]
+        public void When_Product_B_Is_Scanned_The_Value_Of_30_Is_Returned()
+        {
+            var cart = new Checkout();
+            cart.Scan("B");
+
+            Assert.AreEqual(30, cart.GetTotal());
+        }
     }
 }
--- Kata.Cart/Checkout.cs
+++ Kata.Cart/Checkout.cs
@@ -2,14 +2,26 @@
 {
     public class Checkout : ICheckout
     {
+        private int total = 0;
+
         public void Scan(string sku)
         {
-            
+            switch(sku)
+            {
+                case "A":
+                    total += 50;
+                    break;
+                case "B":
+                    total += 30;
+                    break;
+                default:
+                    break;
+            }
         }
 
         public int GetTotal()
         {
-            return 50;
+            return total;
         }
     }
 }
