3733f41fc5064d47d3052c1435bdfca9003c0acf
Paul Kennedy <paulk85@outlook.com>
2015-06-08 18:34:59 +0100

    NUnit and first test added

---
+++ Kata.Cart.Tests/CheckoutTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Kata.Cart.Tests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void When_Product_A_Is_Scanned_The_Value_Of_50_Is_Returned()
+        {
+            decimal result = 50;
+
+            Assert.AreEqual(50, result);
+        }
+    }
+}
